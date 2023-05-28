5c1388b2d91d45ba5da38a297a4ae1a1fc42e99c
Phuong NGUYEN DUY <pnguyenduy@mantu.com>
2023-05-28 16:09:15 +0700

    apply fluentvalidation

--- Sale.Application/DTO/Customers/AddOrUpdateCustomerDTO.cs
+++ Sale.Application/DTO/Customers/AddOrUpdateCustomerDTO.cs
@@ -1,10 +1,24 @@
-﻿namespace Sale.Application.DTO.Customers
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+
+namespace Sale.Application.DTO.Customers
 {
     public class AddOrUpdateCustomerDTO
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime DOB { get; set; }
+        [Required]
         public string Email { get; set; }
+    }
+
+    public class AddOrUpdateStudentValidator : AbstractValidator<AddOrUpdateCustomerDTO>
+    {
+        public AddOrUpdateStudentValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.DOB).NotEmpty().WithMessage("Date of birth is required");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email is invalid");
+        }
     }
 }
